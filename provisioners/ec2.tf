fa6524c97ffd9091f7d262043c07925118e8f1c5
KZeelanBasha <kzeelanbasha@gmail.com>
2025-06-12 11:35:39 +0530

    provisioners

--- provisioners/ec2.tf
+++ provisioners/ec2.tf
@@ -8,6 +8,33 @@ resource "aws_instance" "roboshop" {
   provisioner "local-exec" {
     command = "echo ${self.private_ip} > inventory"
   }
+  
+  provisioner "local-exec" {
+    command = "echo 'instance is destroyed'"
+    when    = destroy
+  }
+
+  connection{
+    type    = "ssh"
+    user    = "ec2-user"
+    password = "DevOps321"
+    host     = self.public_ip
+  }
+
+  provisioner "remote-exec"{
+    inline = [
+      "sudo dnf install nginx -y",
+      "sudo systemctl start nginx"
+    ]
+  }
+
+  provisioner "remote-exec"{
+    when    = destroy
+    inline  = [
+      "sudo systemctl stop nginx"
+    ]
+  }
+
 }
 
 resource "aws_security_group" "allow" {
